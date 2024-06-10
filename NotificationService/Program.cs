037aed76837f57d52b7cfe507f966e7cb2bc251f
Алёна Алёна <alenchaeto1@outlook.com>
2024-06-10 17:17:18 +0300

    fix. last

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.IO;
 using System.Threading.Tasks;
 using System.Net.Mail;
+using System.Net.Mime;
 
 namespace NetConsoleApp
 {
@@ -12,39 +13,21 @@ namespace NetConsoleApp
     {
         static void Main(string[] args)
         {
-            //smtp сервер
-            string smtpHost = "smtp.gmail.com";
-            //smtp порт
-            int smtpPort = 587;
-            //логин
-            string login = "alenchaeto@gmail.com";
-            //пароль
-            string pass = "-seKtd2S3N9e1";
-            
-            // подключение
-            SmtpClient client = new SmtpClient(smtpHost, smtpPort);
-            client.EnableSsl = true;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential(login, pass);
-            
-            // отправитель 
-            MailAddress from = new MailAddress("alenchaeto@gmail.com", "Tom");
-            // кому 
-            MailAddress to = new MailAddress("alenchaeto@mail.ru");
-            
-            //  сообщениe
-            MailMessage m = new MailMessage(from, to);
-            // тема письма
-            m.Subject = "Тест";
-            // текст письма
-            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
-            // письмо представляет код html 
-            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.mail.ru", 587);
+            smtp.Credentials = new System.Net.NetworkCredential("alenchaeto@mail.ru", "JMvwj6tD3r6ACGypqLNq");
+            smtp.EnableSsl = true;
 
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("alenchaeto@mail.ru");
+            mail.To.Add(new MailAddress("alenchaeto@mail.ru"));
+            mail.Subject = "Subject";
+            mail.Body = "Body";
+           
+            smtp.Send(mail);
+            
             try
             {
-                client.Send(m);
+                smtp.Send(mail);
                 Console.WriteLine("Message send");
             }
             catch (Exception ex)
