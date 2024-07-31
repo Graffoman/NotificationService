d7c2f4930f947d0e25e4ecd168ee1b3802bed7ef
AlonaVasileva <vsemposhapke@gmail.com>
2024-07-31 18:46:42 +0300

    fix. параметры доб

--- NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
+++ NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ffaea449b5516b49e2ae8f31b42bf6223132a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d094d9add893a3bcddc4552a38ac53f7e9c0519")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
