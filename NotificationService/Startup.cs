edb9ac06eb4c5906c7d764c3dff4313f50df58a3
izhuravlev <izhuravlev@crimtan.com>
2024-09-06 11:49:20 +0100

    fix. закомментила startup

--- NotificationService/Startup.cs
+++ NotificationService/Startup.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace NotificationService
 {
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.Configure<RabbitSettings>(Configuration.GetSection("RabbitMQ"));
-        services.AddHostedService<RabbitMqEmailConsumer>();
-        // Register other services, including your email service
-    }
+    //public void ConfigureServices(IServiceCollection services)
+    //{
+     //   services.Configure<RabbitSettings>(Configuration.GetSection("RabbitMQ"));
+      //  services.AddHostedService<RabbitMqEmailConsumer>();
+       // // Register other services, including your email service
+    //}
 }
