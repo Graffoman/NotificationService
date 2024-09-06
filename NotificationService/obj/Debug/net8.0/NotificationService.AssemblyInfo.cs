1b16fbbdfbea9ad7b7f3bba3062124d8231be379
izhuravlev <izhuravlev@crimtan.com>
2024-09-06 11:47:00 +0100

    fix. rabbit before program start

--- NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
+++ NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01409cb5869ac8581180c3988ced8c83a4915c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c5d573b3e99e25a12b3a6e520f868045b1710d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
